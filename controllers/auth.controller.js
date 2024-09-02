a97d94dd33479f43027c2eaee94059bdeef8c51d
Diego de Souza Santana <diegossantana068@gmail.com>
2024-09-02 18:41:50 -0300

    authentication pt.2

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,49 @@
 import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
     const {username, email, password} = req.body;
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await prisma.user.create({
+            data: {
+                username, 
+                email, 
+                password: hashedPassword
+            }
+        });
+
+        console.log(newUser);
+
+        res.status(201).json({message: "Usuário criado com sucesso!"});
+
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({message: "Falha ao criar usuário!"})
+    }
 }
 
-export const login = (req, res) => {
-    
+export const login = async (req, res) => {
+    const {username, password} = req.body;
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {username: username}
+        })
+
+        if (!user) return res.status(401).json({message: "Credenciais Inválidas!"});
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) return res.status(401).json({message: "Credenciais Inválidas!"});
+        
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: "Falha ao autenticar o usuário!"})
+    }
 }
 
 export const logout = (req, res) => {
