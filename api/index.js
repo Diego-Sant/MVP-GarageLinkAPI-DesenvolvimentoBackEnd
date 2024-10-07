9c8c8302d1cc39a853de8a57a920e693f846cc9d
Diego de Souza Santana <diegossantana068@gmail.com>
2024-10-07 14:30:07 -0300

    Revert "index.js upgrade"
    This reverts commit 4c6d7ac46247931b0acbb5ae9fb17d961efb2715.

--- api/index.js
+++ api/index.js
@@ -11,31 +11,6 @@ import messageRoute from "../routes/message.route.js"
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const whitelist = [
-    '*'
-];
-  
-app.use((req, res, next) => {
-    const origin = req.get('referer');
-    const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-    if (isWhitelisted) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
-        res.setHeader('Access-Control-Allow-Credentials', true);
-    }
-
-    if (req.method === 'OPTIONS') res.sendStatus(200);
-    else next();
-});
-
-const setContext = (req, res, next) => {
-    if (!req.context) req.context = {};
-    next();
-};
-
-app.use(setContext);
-
 app.use(cors({origin: process.env.CLIENT_URL, credentials: true}));
 app.use(express.json());
 app.use(cookieParser())
