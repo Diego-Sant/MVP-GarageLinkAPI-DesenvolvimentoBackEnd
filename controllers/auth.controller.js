291935ce1a7e9db69b9df28f43f714f70e747cc7
Diego de Souza Santana <diegossantana068@gmail.com>
2024-10-07 15:23:18 -0300

    secure fix

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -51,7 +51,7 @@ export const login = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: true,
+            secure: process.env.NODE_ENV === "production",
             maxAge: age
         }).status(200).json(userInfo);
 
